70a2c87a66f64b59ad0e8eff9b8a87873f1fb52f
Yonas Mekonnen <myonas886@gmail.com>
2025-05-26 12:04:38 +0300

    bug fix test failed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -35,6 +35,4 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that access_nested_map raises KeyError with correct message."""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(
-            str(context.exception), repr(path[len(context.exception.args[0]) - 1])
-        )
+        self.assertEqual(str(context.exception), repr(path[-1]))
