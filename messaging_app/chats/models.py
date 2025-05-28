312d284974ae72df94f1ff1b8ed9b430626c3728
Yonas Mekonnen <myonas886@gmail.com>
2025-05-28 15:43:31 +0300

    Refactor code structure for improved readability and maintainability

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="conversations"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversion {self.id}"
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages"
+    )
+
+    content = models.TextField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message from {self.sender} in Conversation {self.conversation.id}"
