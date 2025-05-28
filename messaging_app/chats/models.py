60f64653f46f14c1e46b5350105f865a6bfcfa28
Yonas Mekonnen <myonas886@gmail.com>
2025-05-28 15:57:53 +0300

    Refactor User, Conversation, and Message models for improved structure and clarity

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,38 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+import uuid
 
 # Create your models here.
 
 
 class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
-        return self.username
+        return f"{self.first_name} {self.last_name} ({self.email})"
 
 
 class Conversation(models.Model):
-    participants = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, related_name="conversations"
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
     )
+    participants = models.ManyToManyField(User, related_name="chats")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversion {self.id}"
+        return f"Conversation {self.conversation_id}"
 
 
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sent_messages"
+        User, on_delete=models.CASCADE, related_name="messages_sent"
     )
     conversation = models.ForeignKey(
         Conversation, on_delete=models.CASCADE, related_name="messages"
     )
-
     content = models.TextField()
-    time_stamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Message from {self.sender} in Conversation {self.conversation.id}"
+        return f"Message {self.message_id} by {self.sender.email}"
