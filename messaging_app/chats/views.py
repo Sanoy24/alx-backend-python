352e81a025cd29b9ece9c23a3b193c80744eb61c
Yonas Mekonnen <myonas886@gmail.com>
2025-06-01 10:50:43 +0300

    Refactor URL routing and enhance Conversation and Message viewsets with improved filtering and response handling

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, filters
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -8,20 +9,40 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all().prefetch_related("participants", "messages")
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["participants__email", "participants__first_name"]
 
     def get_queryset(self):
         user = self.request.user
         return self.queryset.filter(participants=user)
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(response.data, status=status.HTTP_201_CREATED)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all().prefetch_related("sender", "conversation")
+    queryset = Message.objects.all().select_related("sender", "conversation")
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["message_body"]
+    ordering_fields = ["sent_at"]
 
     def get_queryset(self):
         user = self.request.user
-        return self.queryset.filter(conversation__participants=user)
+        queryset = self.queryset.filter(conversation__participants=user)
+
+        # Optional filtering by conversation ID
+        conversation_id = self.request.query_params.get("conversation")
+        if conversation_id:
+            queryset = queryset.filter(conversation__conversation_id=conversation_id)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(response.data, status=status.HTTP_201_CREATED)
