79e2b5ea8d12ffb726fada4eb6b70b4b73c22ae7
Yonas Mekonnen <myonas886@gmail.com>
2025-06-17 10:04:49 +0300

    Add Django Filters and Pagination for Message model

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "chats",
     "rest_framework_simplejwt",
+    "django_filters",
 ]
 
 REST_FRAMEWORK = {
@@ -52,6 +53,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",
+    "PAGE_SIZE": 20,
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
 
