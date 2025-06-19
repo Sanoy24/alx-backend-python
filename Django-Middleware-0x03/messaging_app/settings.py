b6d6402b2c8d8641f7ae12de04d0c8e360c9f246
Yonas Mekonnen <myonas886@gmail.com>
2025-06-19 14:04:45 +0300

    Add RequestLoggingMiddleware to log incoming HTTP requests

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "chats.middleware.RequestLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "messaging_app.urls"
